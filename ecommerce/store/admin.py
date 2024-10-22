c98c51271083125b86180d125f565bc746e41961
Ana Carolyne <anasouza.developer@gmail.com>
2024-10-22 19:44:01 -0300

    subindo as models com o primeiro front

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'digital', 'delete_product')
+    list_filter = ('digital', 'delete_product')
+    search_fields = ('name',)
+
+    def delete_marked_products(self, request, queryset):
+        # Filtrar apenas os produtos marcados para exclusão
+        products_to_delete = queryset.filter(delete_product=True)
+        count = products_to_delete.count()
+        products_to_delete.delete()
+        self.message_user(request, f'{count} produtos foram deletados.')
--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,5 +4,7 @@ from store import views
 urlpatterns = [
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout")
+    path('checkout/', views.checkout, name="checkout"),
+    path('store/', views.store, name='store'),
+
 ]
