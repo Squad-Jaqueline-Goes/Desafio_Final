b4e88b7a96c31383678e1a3b93c50dc7e005f3b6
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2024-10-24 20:33:24 -0300

    Implementando página, rotas e lógica que exibem os detalhes de um produto específico

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     path('store/', views.store, name='store'),
+    path('store/<int:id_product>/', views.product_detail, name="product_detail"),
 
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import *
-
-
+from django.shortcuts import render, get_object_or_404  
 
 def store(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store/store.html', context)
+
+def product_detail(request, id_product):
+    product = get_object_or_404(Product, id=id_product)
+    context = {'product': product}
+    return render(request, 'store/product_detail.html', context)
+
 
 def cart(request):
     context = {}
