b9867c6ed3e6a247573512c9c758f24adf57cf0a
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2024-10-24 22:07:30 -0300

    Implementando Adição de produtos no carrinho de compras

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,6 +4,7 @@ from store import views
 urlpatterns = [
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),    
     path('checkout/', views.checkout, name="checkout"),
     path('store/', views.store, name='store'),
     path('store/<int:id_product>/', views.product_detail, name="product_detail"),
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import *
-from django.shortcuts import render, get_object_or_404  
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
+
 
 def store(request):
     products = Product.objects.all()
@@ -12,11 +14,37 @@ def product_detail(request, id_product):
     context = {'product': product}
     return render(request, 'store/product_detail.html', context)
 
-
-def cart(request):
-    context = {}
-    return render(request, 'store/cart.html', context)
-
 def checkout(request):
     context = {}
-    return render(request, 'store/checkout.html', context)
+    return render(request, 'cart/checkout.html', context)
+
+def get_cart(request):
+    customer = request.user.customer  
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)  
+    return order
+
+def cart(request):
+    cart = get_cart(request)
+    order_items = cart.orderitem_set.all()
+    print(f"Order Items: {order_items}")  # Adicione isso para depuração
+    context = {
+        'cart': cart,
+        'order_items': order_items,
+    }
+    return render(request, 'cart/cart.html', context)
+
+
+def add_to_cart(request, product_id):
+    if request.user.is_authenticated:
+        order = get_cart(request)  
+        product = get_object_or_404(Product, id=product_id)
+        order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+        if not created:
+            order_item.quantity += 1  
+        order_item.save()  
+        return redirect('cart')
+    else:
+        messages.error(request, "Você precisa estar logado para adicionar itens ao carrinho.")
+        return render(request, 'cart/checkout.html')
+
