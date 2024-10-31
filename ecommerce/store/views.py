6a715b7ca08de7b38a337f1c71ac5b71b68f9bca
Ana Carolyne <anasouza.developer@gmail.com>
2024-10-31 01:29:43 -0300

    resolvendo bug do stock

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -133,6 +133,19 @@ def finalize_order(request):
     return render(request, 'cart/checkout.html', context)
 
 
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/checkout.html', context)
+
+
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
